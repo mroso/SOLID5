1ce8d1410340b88d3aab91806198f1a9c2c02c59
mroso <mariana.roso.mol@gmail.com>
2020-05-22 20:23:17 -0300

    Ejercicio 5 DIP
    commiteo la solucion del ejercicio 5

--- src/Library/FilePrinter.cs
+++ src/Library/FilePrinter.cs
@@ -4,7 +4,8 @@ namespace Full_GRASP_And_SOLID
 {
     public class FilePrinter : IPrinter
     {
-        public void PrintRecipe(Recipe recipe)
+        // Modificado por DIP
+        public void PrintRecipe(IRecipeContent recipe)
         {
             File.WriteAllText("Recipe.txt", recipe.GetTextToPrint());
         }
